3ac7e70c9b6314619fcbf44c7c263131fa862d8f
Apon <flapon808@gmail.com>
2023-02-11 17:09:12 +0600

    Error fixing part 2

--- CRUD/example/urls.py
+++ CRUD/example/urls.py
@@ -3,8 +3,9 @@ from django.urls.conf import include
 from example import views
 urlpatterns = [
     path('emp', views.emp),
-    path('show', views.show),
+    path('', views.show),
     path('edit/<int:id>', views.edit),
     path('update/<int:id>', views.update),
     path('delete/<int:id>', views.delete),
+    path('show', views.show),
 ]
